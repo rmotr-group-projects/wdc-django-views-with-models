6860d7bc062928b1abed690c8ba30fa2d8e6c0e9
stephanie taylor <staylor733@hotmail.com>
2019-06-09 19:31:16 +0000

    setting views and models

--- django_views_with_models/artists/views.py
+++ django_views_with_models/artists/views.py
@@ -19,7 +19,20 @@ def artists(request):
         parameter (if its given) and filter all the artists that have a
         popularity greater or equal to the given one.
     """
-    pass
+    artists = Artist.objects.all()
+
+    first_name = request.GET.get('first_name')
+    
+    if first_name:
+        artists = artists.filter(first_name__icontains=first_name)
+
+    popularity = request.GET.get('popularity')
+    
+    if popularity:
+        artists = artists.filter(popularity__gte=popularity)
+
+    return render(request, 'artists.html', context={'artists': artists})
+
 
 
 def artist(request, artist_id):
@@ -29,7 +42,12 @@ def artist(request, artist_id):
         the DB. Then render the 'artist.html' template sending the 'artist'
         object as context
     """
-    pass
+    
+    try:
+        artist = Artist.objects.get(id=artist_id)
+    except Artist.DoesNotExist:
+        return HttpResponseNotFound()
+    return render(request, 'artist.html', context={'artist': artist})
 
 
 def songs(request, artist_id=None):
@@ -51,4 +69,17 @@ def songs(request, artist_id=None):
         songs that match with given artist_id and render the same 'songs.html'
         template.
     """
-    pass
+    songs = Song.objects.all()
+    
+    if artist_id:
+        songs = songs.filter(artist_id=artist_id)
+
+    title = request.GET.get('title')
+    if title:
+        songs = songs.filter(title__icontains=title)
+
+    for song in songs:
+        artist = Artist.objects.get(id=song.artist_id)
+        song.artist = artist
+
+    return render(request, 'songs.html', context={'songs': songs})
