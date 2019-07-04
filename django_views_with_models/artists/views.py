f901d95ac2d63d790667aa3c6129c63902672207
Ilya Koy <Ilya.koy@outlook.com>
2019-07-04 08:28:57 -0400

    tasks working

--- django_views_with_models/artists/views.py
+++ django_views_with_models/artists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseNotFound
 
 from .models import Artist, Song
@@ -19,7 +19,20 @@ def artists(request):
         parameter (if its given) and filter all the artists that have a
         popularity greater or equal to the given one.
     """
-    pass
+    artist_objects = Artist.objects.all()
+
+    #Check GET for first_name param
+    get_first_name = request.GET.get("first_name")
+    if get_first_name:
+        artist_objects = artist_objects.filter(first_name__icontains=get_first_name)
+
+    #Check popularity
+    get_popularity = request.GET.get("popularity")
+    if get_popularity:
+        artist_objects = artist_objects.filter(popularity__gte=get_popularity)
+
+    artist_contex = {'artists':artist_objects}
+    return render(request, 'artists.html',artist_contex)
 
 
 def artist(request, artist_id):
@@ -29,7 +42,12 @@ def artist(request, artist_id):
         the DB. Then render the 'artist.html' template sending the 'artist'
         object as context
     """
-    pass
+    artist_object_by_id = get_object_or_404(Artist, id=artist_id)
+    artsit_object_context = {
+        'artist':artist_object_by_id
+    }
+    return render(request,'artist.html',artsit_object_context)
+
 
 
 def songs(request, artist_id=None):
@@ -51,4 +69,20 @@ def songs(request, artist_id=None):
         songs that match with given artist_id and render the same 'songs.html'
         template.
     """
-    pass
+    song_objects = Song.objects.all()
+
+    get_title = request.GET.get('title')
+    if get_title:
+        song_objects = song_objects.filter(title__icontains=get_title)
+
+    if artist_id:
+        song_objects = song_objects.filter(artist_id=artist_id)
+
+    for song in song_objects:
+        artist = Artist.objects.get(id=song.artist_id)
+        song.artist = artist
+    
+    songs_context = {'songs':song_objects}
+
+    return render(request,"songs.html",songs_context)
+
